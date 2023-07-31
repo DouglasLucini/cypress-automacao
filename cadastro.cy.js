93c219d2080cc39f2b73e3d68d41279fe78bf7b1
ADM-CESUMAR\douglas.correa <douglas.correa@unicesumar.edu.br>
2023-07-31 15:07:43 -0300

    Pronto

--- cadastro.cy.js
+++ cadastro.cy.js
@@ -4,11 +4,11 @@ describe ('Cadastro', () =>{
         it('Usuário deve se conectar', () => {
             
             cy.viewport(1440, 900)
-            cy.visit('https://mundoazul.unicesumar.edu.br')
+            cy.visit('https://mundoazul-dev.unicesumar.edu.br')
             cy.get('#loginUsuario div div div input').type('douglas.correa')
             cy.get('#loginSenha div div div input').type ('Dgs##40506098')
             cy.get ('#submit-button span p').click()
-            cy.get('#mat-dialog-0 app-perfil-selector div div div div img').click()
+            cy.contains('Perfil Completo').click()
         })
 
 
